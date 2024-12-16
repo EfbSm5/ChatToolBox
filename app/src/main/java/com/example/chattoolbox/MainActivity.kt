a079f4b5c61845e8f270f90a4da357c79b0f6194
EfbSm5 <3325989547@qq.com>
2024-12-16 22:39:05 +0800

    initial commit

---
+++ app/src/main/java/com/example/chattoolbox/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.chattoolbox
+
+import android.os.Bundle
+import android.speech.SpeechRecognizer
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.chattoolbox.ui.theme.ChatToolBoxTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChatToolBoxTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android", modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val can = SpeechRecognizer.isRecognitionAvailable(LocalContext.current)
+    Text(
+        text = "Hello $name!,$can", modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ChatToolBoxTheme {
+        Greeting("Android")
+    }
+}
